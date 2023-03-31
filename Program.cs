624f408853f94a50155561b0be20c6c8d64ba0c1
Ganbar Guluyev <ganbarguluyev@gmail.com>
2023-03-31 15:49:57 +0300

    fead: add title

--- Program.cs
+++ Program.cs
@@ -11,8 +11,14 @@ namespace Milyoner
                 //  Kim Milyoner Olmak İster
 
             Console.WriteLine(" Kim Milyoner Olmak İster \n ");
-           
-            
+            Console.Write(" İsim Giriniz: ");
+            string isim = Console.ReadLine();
+            Console.Write(" Soyisim Girin: ");
+            string soyisim = Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine(" Hoş Geldin: " + isim + " " + soyisim);
+
+
         }
     }
 }
